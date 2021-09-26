b3a88ddf3a3fc8790f46c35dbe2cfa6265144a93
wnsgud0428 <wnsgud0428@naver.com>
2021-09-26 20:24:52 +0900

    webcam 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
+// eslint-disable-next-line
 import logo from './logo.svg';
 import './App.css';
+// eslint-disable-next-line
+import * as tf from "@tensorflow/tfjs";
+// eslint-disable-next-line
+import * as posenet from "@tensorflow-models/posenet";
+import Webcam from "react-webcam";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        "Hello World2!"
+        <Webcam
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
       </header>
     </div>
   );
