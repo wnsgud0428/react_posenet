bccca60e556ae26e2d20bb4709018107bae19ea7
wnsgud0428 <wnsgud0428@naver.com>
2021-11-21 20:48:14 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useRef } from "react";
-import './App.css';
+import "./App.css";
 import * as tf from "@tensorflow/tfjs";
 import * as posenet from "@tensorflow-models/posenet";
 import Webcam from "react-webcam";
 import { drawKeypoints, drawSkeleton } from "./utilities";
-import imgA from './images/wrong2.jpg'
+import imgA from "./images/wrong2.jpg";
 
 function App() {
   const [imgSrc, setImgSrc] = React.useState(null); //capture를 위해서?
@@ -17,10 +17,10 @@ function App() {
   //  Load posenet
   const runPosenet = async () => {
     const net = await posenet.load({
-      architecture: 'ResNet50',
+      architecture: "ResNet50",
       outputStride: 32,
       inputResolution: { width: 257, height: 200 },
-      quantBytes: 2
+      quantBytes: 2,
     });
     //
     setInterval(() => {
@@ -34,16 +34,12 @@ function App() {
       webcamRef.current !== null &&
       webcamRef.current.video.readyState === 4
     ) {
-
       // Get Video Properties
-      const video = webcamRef.current.video
+      const video = webcamRef.current.video;
       const videoWidth = webcamRef.current.video.videoWidth;
       const videoHeight = webcamRef.current.video.videoHeight;
 
-
-
-
-      const imgid = document.getElementById('testimg');
+      const imgid = document.getElementById("testimg");
       const imageWidth = imgid.width;
       const imageHeight = imgid.height;
       imgid.height = 480;
@@ -52,7 +48,6 @@ function App() {
       // Set video width
       webcamRef.current.video.width = videoWidth;
       webcamRef.current.video.height = videoHeight;
-
 
       // Make Detections
       const pose = await net.estimateSinglePose(imgid);
@@ -89,25 +84,44 @@ function App() {
 
   runPosenet();
 
-
-
   return (
     <div className="App">
       <div>
-        <h1>
-          라이브 비디오 + PoseNet
-        </h1>
+        <h1>라이브 비디오 + PoseNet</h1>
       </div>
       <div>
         스쿼트 횟수:
         <span id="count"></span>
       </div>
       <div>
-        <img ref={imageRef} id='testimg' src={imgA} width='640' height='480' alt='testA' />
+        <img
+          ref={imageRef}
+          id="testimg"
+          src={imgA}
+          width="640"
+          height="480"
+          alt="testA"
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
       </div>
       <div>
-        <Webcam
-          ref={webcamRef}
+        <img
+          ref={imageRef}
+          id="testimg"
+          src={imgA}
+          width="640"
+          height="480"
+          alt="testA"
           style={{
             position: "absolute",
             marginLeft: "auto",
@@ -136,7 +150,6 @@ function App() {
           }}
         />
       </div>
-
     </div>
   );
 }
